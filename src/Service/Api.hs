7582d7bf84415c957de14c427e6de99e56f0876c
Dan Palmer <dan@danpalmer.me>
2016-05-21 15:02:12 +0100

    Really simple handler with some validation

--- src/Service/Api.hs
+++ src/Service/Api.hs
@@ -6,14 +6,43 @@ module Service.Api where
 
 import Servant
 import Data.Text
+import qualified Data.Text as T
+import Control.Monad.IO.Class
 
 import Types (AppM)
 
+import Service.BT
+
 --- API
 
-type ServiceAPI = Get '[PlainText] Text
+type ServiceAPI = "lookup" :> ReqBody '[FormUrlEncoded] Query :> Post '[PlainText] Text
+
+--- Model
+
+data Query = Query {
+      postcode :: Text
+    , address :: Text
+    } deriving (Eq, Show)
+
+instance FromFormUrlEncoded Query where
+    fromFormUrlEncoded inputs =
+        Query
+            <$> field "postcode"
+            <*> field "address"
+        where
+            field label = case lookup label inputs of
+                Nothing -> Left $ "Field " ++ (unpack label) ++ " not found"
+                Just value -> validate label value
+            validate label value = case (T.null value) of
+                True -> Left $ "Field " ++ (unpack label) ++ " must not be empty"
+                False -> Right value
 
 --- Handlers
 
 serviceAPIHandler :: ServerT ServiceAPI AppM
-serviceAPIHandler = return "Hello, world!"
+serviceAPIHandler = lookupHandler
+
+lookupHandler :: Query -> AppM Text
+lookupHandler query = do
+    liftIO $ putStrLn $ show $ query
+    return "Hello, world!"
