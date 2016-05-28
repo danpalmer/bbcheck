a8d1288c84419f3766076e7391b400f66043fa79
Dan Palmer <dan@danpalmer.me>
2016-05-28 14:26:50 +0100

    Integrate providers and use JSON

--- src/Service/Api.hs
+++ src/Service/Api.hs
@@ -1,48 +1,75 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Service.Api where
 
+import GHC.Generics
 import Servant
-import Data.Text
+import Data.Aeson
+import Data.Either (rights)
 import qualified Data.Text as T
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class (liftIO)
 
 import Types (AppM)
 
-import Service.BT
+import qualified Service.BT as BTUtils
+import qualified Service.VirginMedia as VirginMediaUtils
+
+import Service.Types
 
 --- API
 
-type ServiceAPI = "lookup" :> ReqBody '[FormUrlEncoded] Query :> Post '[PlainText] Text
+type ServiceAPI = "lookup" :> ReqBody '[JSON] Query :> Post '[JSON] LookupResult
 
 --- Model
 
 data Query = Query {
-      postcode :: Text
-    , address :: Text
-    } deriving (Eq, Show)
+      postcode :: String
+    , address :: String
+} deriving (Eq, Show, Generic)
 
-instance FromFormUrlEncoded Query where
-    fromFormUrlEncoded inputs =
-        Query
-            <$> field "postcode"
-            <*> field "address"
-        where
-            field label = case lookup label inputs of
-                Nothing -> Left $ "Field " ++ (unpack label) ++ " not found"
-                Just value -> validate label value
-            validate label value = case (T.null value) of
-                True -> Left $ "Field " ++ (unpack label) ++ " must not be empty"
-                False -> Right value
+instance FromJSON Query
+
+data LookupError = LookupError {
+      provider :: Provider
+    , message :: String
+} deriving (Generic)
+
+instance ToJSON LookupError
+
+data LookupResult = LookupResult {
+      options :: [InternetOption]
+    , errors :: [LookupError]
+} deriving (Generic)
+
+instance ToJSON LookupResult
 
 --- Handlers
 
 serviceAPIHandler :: ServerT ServiceAPI AppM
 serviceAPIHandler = lookupHandler
 
-lookupHandler :: Query -> AppM Text
+lookupHandler :: Query -> AppM LookupResult
 lookupHandler query = do
-    liftIO $ putStrLn $ show $ query
-    return "Hello, world!"
+    responses <- liftIO serviceResponses
+    let results = formatResults responses
+    let errors = formatErrors responses
+    return $ LookupResult {options = results, errors = errors}
+        where
+            serviceLookups :: [(Provider, String -> String -> IO (Either String [InternetOption]))]
+            serviceLookups = [ (VirginMedia, VirginMediaUtils.getInternetOptionsForPostcode)
+                             , (OpenReach, BTUtils.getInternetOptionsForPostcode)
+                             ]
+
+            serviceResponses :: IO [(Provider, Either String [InternetOption])]
+            serviceResponses = flip mapM serviceLookups $ \(p, fn) -> do
+                optionOrError <- fn (postcode query) (address query)
+                return (p, optionOrError)
+
+            formatErrors :: [(Provider, Either String [InternetOption])] -> [LookupError]
+            formatErrors responses = [LookupError p e | (p, Left e) <- responses]
+
+            formatResults :: [(Provider, Either String [InternetOption])] -> [InternetOption]
+            formatResults = concat . rights . (map snd)
